c5bbf4943b20d838de779a166621c4bf6bd91989
Xenijo <80117820+Xenijo@users.noreply.github.com>
2023-04-30 15:49:25 +0200

    Update TripHubApiMain.lua

--- TripHubApiMain.lua
+++ TripHubApiMain.lua
@@ -1,4 +1,4 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/elxocasXD/ww/main/Zzz%20f"))()
 
 local services = {
     Workspace = game:GetService("Workspace"),
