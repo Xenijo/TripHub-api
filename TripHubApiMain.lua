c9c14267b63b6af2013e623f8fbd7ef14cb4585e
Xenijo <80117820+Xenijo@users.noreply.github.com>
2023-04-30 14:49:56 +0200

    Create TripHubApiMain.lua

---
+++ TripHubApiMain.lua
@@ -0,0 +1,37 @@
+local services = {
+    Workspace = game:GetService("Workspace"),
+    ReplicatedStorage = game:GetService("ReplicatedStorage"),
+    ServerStorage = game:GetService("ServerStorage"),
+    Lighting = game:GetService("Lighting"),
+    Players = game:GetService("Players"),
+    StarterPlayer = game:GetService("StarterPlayer"),
+    StarterPack = game:GetService("StarterPack"),
+    SoundService = game:GetService("SoundService"),
+    HttpService = game:GetService("HttpService"),
+    RunService = game:GetService("RunService"),
+    UserInputService = game:GetService("UserInputService"),
+    ContextActionService = game:GetService("ContextActionService"),
+    MarketplaceService = game:GetService("MarketplaceService"),
+    TeleportService = game:GetService("TeleportService"),
+    InsertService = game:GetService("InsertService"),
+    ContentProvider = game:GetService("ContentProvider"),
+    LocalizationService = game:GetService("LocalizationService"),
+    PathfindingService = game:GetService("PathfindingService"),
+    GroupService = game:GetService("GroupService"),
+    AnalyticsService = game:GetService("AnalyticsService"),
+    DataStoreService = game:GetService("DataStoreService"),
+    MessagingService = game:GetService("MessagingService"),
+    PhysicsService = game:GetService("PhysicsService"),
+    ReplicatedFirst = game:GetService("ReplicatedFirst"),
+    TextService = game:GetService("TextService"),
+    TweenService = game:GetService("TweenService"),
+    WorkspaceEdit = game:GetService("WorkspaceEdit"),
+}
+
+local Api = {}
+
+for name, service in pairs(services) do
+    Api[name] = service
+end
+
+return Api
